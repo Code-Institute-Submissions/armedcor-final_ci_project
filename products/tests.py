5dfc8193801cc1bd2c9f8a67487a0198b199418f
Cormac Lawlor <armedcor@gmail.com>
2019-09-02 15:42:08 +0100

    Added product models, linked to django admin and created a test

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+class ProductTests(TestCase):
+    def test_str(self):
+        """ Tests the name of product shows correctly"""
+        test_name = Product(name='A Product')
+        self.assertEqual(str(test_name), 'A Product')
+
+
+
+
