1f5eb4a9f9a0e747f90151b1b2cdcb64953e5496
Cormac Lawlor <armedcor@gmail.com>
2019-09-03 15:33:04 +0100

    built up session only cart that enables view cart, add to cart and adjust cart

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,10 +17,10 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from accounts import urls as urls_accounts
 from products import urls as urls_products
+from cart import urls as urls_cart
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
-
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -28,5 +28,6 @@ urlpatterns = [
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
+    url(r'^cart/', include(urls_cart)),
 
 ]
