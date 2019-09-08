e28af6478b900799822b2b093abbd5b4c2143bc5
Cormac Lawlor <armedcor@gmail.com>
2019-09-09 00:02:21 +0100

    Added Heroku to allowed hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['six-string-king.herokuapp.com']
 
 # Application definition
 
